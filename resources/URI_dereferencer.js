9d05cbef86d85844436b90b33717b60ed94f56a9
Alex <alexrandles0@gmail.com>
2020-01-29 21:39:45 +0000

    implemented dereferencing and further checking

--- resources/URI_dereferencer.js
+++ resources/URI_dereferencer.js
@@ -1,6 +1,8 @@
 var NS = "http://www.w3.org/ns/r2rml#";
 var datatype_URI_checked = false;
 var class_URI_checked = false;
+var predicate_URI_checked = false;
+
 
 function dereferenceClassURI(resource) {
 	var labelProperty = TermFactory.namedNode(NS+"class");
@@ -22,6 +24,28 @@ function dereferenceClassURI(resource) {
 		}
 	}
 }
+
+function dereferencePredicateURI(resource) {
+	var labelProperty = TermFactory.namedNode(NS+"predicate");
+	var labels = $data.find(resource, labelProperty, null);
+	for(;;) {
+		var labelTriple = labels.next();
+		print(labelTriple);
+		if(!labelTriple) {
+			return null;
+		}
+		if (!predicate_URI_checked){
+		    var JavaCLass = Java.type("JavaClasses.DereferenceURI");
+	        var ResponseCode = JavaCLass.getResponseCode(String(labelTriple.object));
+	        predicate_URI_checked = true;
+            return ResponseCode;
+		}
+		else{
+		    return null;
+		}
+	}
+}
+
 
 function dereferenceDataTypeURI(resource) {
 	var labelProperty = TermFactory.namedNode(NS+"datatype");
