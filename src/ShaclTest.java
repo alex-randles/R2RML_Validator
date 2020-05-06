9a0bcaae820389b04aea22bb442551a8139db401
Alex <alexrandles0@gmail.com>
2020-05-06 12:54:28 +0100

    backup

--- src/ShaclTest.java
+++ src/ShaclTest.java
@@ -5,6 +5,14 @@ import org.apache.jena.rdf.model.*;
 import org.topbraid.shacl.validation.ValidationUtil;
 import java.io.*; 
 
+// not needed 
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.sparql.vocabulary.FOAF;
+import org.apache.jena.update.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 public class ShaclTest {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
@@ -17,6 +25,7 @@ public class ShaclTest {
 			runTest(FileNames.shapesFile, FileNames.originalMappingFile, FileNames.reportFile);
 		}
 		catch (Exception e){
+			System.out.println(e); 
 			String outputMessage = String.format("Problem assessing mapping!!!\n" +
 					"Please ensure '%s' contains a valid R2RML mapping.....", args[0]);
 			System.out.println(outputMessage);
@@ -41,6 +50,38 @@ public class ShaclTest {
 		report.getModel().write(System.out, "TURTLE");
 		File file = new File(output_file);
 		try {
+			String queryString = "SELECT ?focusNode WHERE {?s <http://www.w3.org/ns/shacl#focusNode> ?focusNode } "; 
+			System.out.println(queryString); 
+			QueryExecution qexec = QueryExecutionFactory.create(queryString, report.getModel());
+			                ResultSet results = qexec.execSelect() ;
+                results = ResultSetFactory.copyResults(results) ;
+                qexec.close();
+			            String stringVariable = results.nextSolution().get("?focusNode").toString();
+			            System.out.println("ddhdhdhd");
+			            System.out.println(stringVariable); 
+			            			String query = String.format("PREFIX rr: <http://www.w3.org/ns/r2rml#>" +
+											"SELECT ?s ?p ?o WHERE {_:%s rr:termType ?o   } ", stringVariable) ;
+System.out.println(query); 
+		QueryExecution qexec2 = QueryExecutionFactory.create(query, data);
+			                ResultSet results2 = qexec2.execSelect() ;
+                results2 = ResultSetFactory.copyResults(results2) ;
+                qexec2.close();
+			for ( ; results2.hasNext() ; )
+			{
+				QuerySolution soln = results2.nextSolution() ;
+				System.out.print(soln.get("?s") );
+				System.out.print("  "); 
+								System.out.print(soln.get("?p") );
+												System.out.print("  "); 
+
+								System.out.println(soln.get("?o") );
+
+			}
+
+
+
+
+// testing above 
 			FileOutputStream fop = new FileOutputStream(file);
 			// fop.write(contentInBytes);
 			report.getModel().write(fop, "TURTLE");
@@ -55,6 +96,7 @@ public class ShaclTest {
 			System.out.println(StringUtils.repeat('*', 70));
 
 		} catch (Exception e) {
+			System.out.println(e); 
 			System.out.println("File not found!");
 		}
 	}
